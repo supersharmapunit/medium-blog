d1342afc73115b885439411d427deb7f5a0ee417
punit sharma <punit2000sharma@gmail.com>
2024-04-15 13:24:19 +0530

    added cors support

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from 'hono'
 import apiRoutes from './routes';
+import { cors } from 'hono/cors';
 
 const app = new Hono();
 
+app.use("/*", cors());
 app.route("/api", apiRoutes);
 
 export default app;
